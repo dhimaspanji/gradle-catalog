bf99875a4e7c1f7fe337d2b31b076e81e99fab7f
Dhimas Panji <dhimz777@gmail.com>
2025-07-23 18:05:52 +0700

    initial project

---
+++ src/main/java/id/co/project/dhimas/gradle/catalog/GradleCatalogApplication.java
@@ -0,0 +1,13 @@
+package id.co.project.dhimas.gradle.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/project/dhimas/gradle/catalog/GradleCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.project.dhimas.gradle.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
