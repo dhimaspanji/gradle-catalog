fd20d245d6970bcb4ff7ceb0a19ad0e30c84d4b7
Dhimas Panji <dhimz777@gmail.com>
2025-08-13 14:31:02 +0700

    test disabled button merge before pipeline success - 7

--- src/main/java/id/co/project/dhimas/gradle/catalog/GradleCatalogApplication.java
+++ src/main/java/id/co/project/dhimas/gradle/catalog/GradleCatalogApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GradleCatalogApplication {
 
 	public static void main(String[] args) {
-		System.out.println("TEST DISABLED BUTTON MERGE BEFORE PIPELINE SUCCESSFULLY - 6");
+		System.out.println("TEST DISABLED BUTTON MERGE BEFORE PIPELINE SUCCESS - 7");
 		SpringApplication.run(GradleCatalogApplication.class, args);
 	}
 
